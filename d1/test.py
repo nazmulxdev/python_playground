5a56aa9fc92ddb30443fdf6a1ddf8678f6ecdfe4
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-11-21 21:12:07 +0600

    explore Escape Sequence and print statement

--- d1/test.py
+++ d1/test.py
@@ -1,1 +1,7 @@
-print ("Hi, I am nazmul. Full stack software developer.")
+# This is the starting of this code .
+# Explored: Comment , Escape Sequence and print Statement.
+print ("Hi, I am nazmul \n \"Full stack\"\ software developer.")
+
+print ("Md Nazmul Hossen",285,True,sep="-",end="fuck")
+
+print ("MERN Stack Developer")
